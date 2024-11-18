2010187574eddd5b0c6f1d981882593051483041
Igor <test@gmail.com>
2024-11-18 11:53:05 +0700

    init proj

---
+++ file-watcher/src/main/java/ru/mahotin/FileWatcherApp.java
@@ -0,0 +1,12 @@
+package ru.mahotin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class FileWatcherApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FileWatcherApp.class);
+    }
+
+}
